3c02d06ef9f3bc66acf734c088e87e6dee489ad8
kaizenthebeast <nakan.hamza@gmail.com>
2026-04-11 18:36:52 +0800

    Fix the responsiveness of product card

--- app/(shop)/page.tsx
+++ app/(shop)/page.tsx
@@ -5,8 +5,8 @@ import ProductContainer from "@/components/public/layout/ProductContainer";
 
 export default function Home() {
   return (
-      <div className="flex min-h-svh w-full items-center justify-center  md:py-10">
-        <Suspense fallback={<div>Loading...</div>} >
+      <div className="flex min-h-svh max-w-7xl mx-auto items-center justify-center  md:p-5">
+        <Suspense fallback={<div>Loading...</div>} > 
           <ProductContainer />
         </Suspense>
 
