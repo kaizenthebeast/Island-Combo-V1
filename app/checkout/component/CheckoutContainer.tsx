b5809139bc40e349a92d62e26c3444f039ba2092
kaizenthebeast <nakan.hamza@gmail.com>
2026-04-18 17:11:56 +0800

    fix file path

--- app/checkout/component/CheckoutContainer.tsx
+++ app/checkout/component/CheckoutContainer.tsx
@@ -1,10 +1,10 @@
-import OrderSummaryContainer from '@/components/private/layout/OrderSummaryContainer';
+
 
 const CheckoutContainer = () => {
     return (
         <div className="flex flex-col lg:flex-row gap-6 lg:gap-8 px-4 py-10">
             <div className="w-full lg:w-2/3">
-                <OrderSummaryContainer />
+               
             </div>
             <div>
                 <h2>You must login or sign up first!</h2>
