e295fb657539d507815bbf1dcc19f7a98bd956f9
kaizenthebeast <nakan.hamza@gmail.com>
2026-04-19 15:57:41 +0800

    add addrss schema

--- form-schema/addressSchema.ts
+++ form-schema/addressSchema.ts
@@ -1,7 +1,13 @@
-import {z} from 'zod';
+import { z } from "zod";
 
 export const addressSchema = z.object({
+    firstName: z.string().min(1, "First name is required").trim(),
+    lastName: z.string().min(1, "Last name is required").trim(),
+    phoneNumber: z.string().min(10, "Phone number is too short").max(15, "Phone number is too long").regex(/^[0-9+ ]+$/, "Invalid phone number format"),
+    address: z.string().min(1, "Address is required").trim(),
+    postalCode: z.string().min(4, "Postal code is too short").max(10, "Postal code is too long").regex(/^[0-9A-Za-z-]+$/, "Invalid postal code format"),
+    locality: z.string().min(1, "Locality is required").trim(),
+    country: z.string().min(1, "Country is required").trim(),
+});
 
-})
-
-export type AddressFormInput = z.infer<typeof addressSchema>
+export type AddressFormInput = z.infer<typeof addressSchema>;
